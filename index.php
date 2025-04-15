6aa84aebdb5971d23b5d0d4884dfa5aa8cdfbd87
finding-nino <stekkerdoes@hotmail.com>
2025-04-15 11:00:13 +0200

    dadadad

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QR Code Generator</title>
+    <title>Ticket Info</title>
     <!-- Include QR code library -->
     <script src="https://cdn.jsdelivr.net/npm/qrcode@1.5.1/build/qrcode.min.js"></script>
     <!-- Include your JavaScript file -->
