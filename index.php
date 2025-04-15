0067c3139cc2f2dfff37361c9d435dacd4cb4b94
finding-nino <stekkerdoes@hotmail.com>
2025-04-15 10:34:29 +0200

    alles required enzo + dob fix
    oehlala

--- index.php
+++ index.php
@@ -44,47 +44,72 @@ ini_set('display_errors', 1);
         <div class="overlay" id="overlay" onclick="toggleMenu()"></div>
     </div>
 
-
-
     <div style="margin-top: 15px"; id="ticket-site-form">
         <form action="db_send.php" method="post">
-            <label for="first_name" >Voornaam:</label><br>
-            <input type="text" id="first_name" name="first_name" class="input"><br>
+            <label for="first_name">Voornaam:</label><br>
+            <input type="text" id="first_name" name="first_name" class="input" required><br>
 
-            <label for="last_name" >Achternaam:</label><br>
-            <input type="text" id="last_name" name="last_name" class="input"><br>
+            <label for="last_name">Achternaam:</label><br>
+            <input type="text" id="last_name" name="last_name" class="input" required><br>
 
-            <label for="date_of_birth" >Geboortedatum:</label><br>
-            <input type="text" id="date_of_birth" name="date_of_birth" class="input"><br>
+            <label>Geboortedatum (DD-MM-JJJJ):</label><br>
+            <div class="date-of-birth-container" style="display: flex; gap: 5px;">
+                <!-- Day dropdown (DD) -->
+                <select name="day" id="day" class="input date-input" required style="width: 80px;">
+                    <option value="">Dag</option>
+                    <?php for ($i = 1; $i <= 31; $i++): ?>
+                        <option value="<?= str_pad($i, 2, '0', STR_PAD_LEFT) ?>">
+                            <?= str_pad($i, 2, '0', STR_PAD_LEFT) ?>
+                        </option>
+                    <?php endfor; ?>
+                </select>
 
-            <label for="email" >Email:</label><br>
-            <input type="text" id="email" name="email" class="input"><br>
+                <!-- Month dropdown (MM) -->
+                <select name="month" id="month" class="input date-input" required style="width: 120px;">
+                    <option value="">Maand</option>
+                    <?php 
+                    $months = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", 
+                              "Juli", "Augustus", "September", "Oktober", "November", "December"];
+                    foreach ($months as $index => $month): 
+                        $monthNumber = str_pad($index + 1, 2, '0', STR_PAD_LEFT);
+                    ?>
+                        <option value="<?= $monthNumber ?>"><?= $month ?></option>
+                    <?php endforeach; ?>
+                </select>
 
-            <label for="phone_number" >Telefoonnummer:</label><br>
-            <input type="text" id="phone_number" name="phone_number" class="input"><br>
+                <!-- Year dropdown (YYYY) -->
+                <select name="year" id="year" class="input date-input" required style="width: 100px;">
+                    <option value="">Jaar</option>
+                    <?php 
+                    $currentYear = date("Y");
+                    for ($i = $currentYear; $i >= 1900; $i--): ?>
+                        <option value="<?= $i ?>"><?= $i ?></option>
+                    <?php endfor; ?>
+                </select>
+            </div><br>
 
-            <label for="adres" >Adres:</label><br>
-            <input type="text" id="adres" name="adres" class="input"><br>
+            <label for="email">Email:</label><br>
+            <input type="text" id="email" name="email" class="input" required><br>
 
-            <label for="bsn" >BSN:</label><br>
-            <input type="text" id="bsn" name="bsn" class="input"><br>
+            <label for="phone_number">Telefoonnummer:</label><br>
+            <input type="text" id="phone_number" name="phone_number" class="input" required><br>
+
+            <label for="address">Adres:</label><br>
+            <input type="text" id="address" name="address" class="input" required><br>
+
+            <label for="bsn">BSN:</label><br>
+            <input type="text" id="bsn" name="bsn" class="input" required><br>
 
             <input type="hidden" name="qr_code_data">
 
-            
-
-            
             <input type="submit" id="submit_knop" value="Versturen" class="input">
             <input type="reset" id="reset_knop" value="Leegmaken" class="input">
-          </form>
+        </form>
     </div>
-    <!--<button onclick="generateQR()" style="margin-top: 15px">Geef ons geld! (grapje is gratis)</button>-->
 
     <div id="qrcode"></div>
     
-<!-- https://chat.deepseek.com/a/chat/s/72e4341b-41c6-4458-b12c-6eac69ced6ad -->
-    
-<?php include "db_send.php"?>
+
     
 </body>
 </html>
